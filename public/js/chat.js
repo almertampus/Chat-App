af13e6bcc2486d83d842f502c6a0db9b2fe6ad99
Almer F. Tampus <aftampus@iskolarngbayan.pup.edu.ph>
2020-08-15 15:57:27 +0800

    Added basic structure of chat app

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,35 @@
-io()
+const socket = io()
+
+socket.on('message', (msg) => {
+    console.log(msg)
+})
+
+document.querySelector('#messageForm').addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    let msg = e.target.elements.msg.value
+
+    socket.emit('replyMessage', msg, () => {
+        console.log('The message was delivered.')
+    })
+
+    // clears the input field after sending the message
+    e.target.elements.msg.value = ''
+})
+
+document.querySelector('#send-location').addEventListener('click', () => {
+    if (!navigator.geolocation) {
+        return alert('Geolocation is not supported by your browser.')
+    }
+
+    navigator.geolocation.getCurrentPosition((position) => {
+        socket.emit('sendLocation', {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        })
+    })
+})
+
+socket.on('location', (location) => {
+    console.log(location)
+})
--- src/index.js
+++ src/index.js
@@ -6,15 +6,35 @@ const socketio = require('socket.io')
 const app = express()
 const server = http.createServer(app)
 const io = socketio(server)
-
 const port = process.env.PORT || 3000
-
 const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
 
-io.on('connection', () => {
+io.on('connection', (socket) => {
     console.log('New WebSocket connection')
+
+    const msg = 'Welcome to the conversation!'
+
+    // detects if a new user has joined, and inform other users in the conversation
+    socket.emit('message', msg)
+    socket.broadcast.emit('message', 'A new user has joined the conversation!')
+
+    // receives a message from a user, and sends back that message to other users in the
+    socket.on('replyMessage', (msg, callback) => {
+        io.emit('message', msg)
+        callback()
+    })
+
+    // receives a location from a user, and sends back that location to other users in the conversation
+    socket.on('sendLocation', (coordinates) => {
+        io.emit('location', `https://google.com/maps?q=${coordinates.latitude},${coordinates.longitude}`)
+    })
+
+    // detects if a user has leave the conversation
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has left the conversation.')
+    })
 })
 
 server.listen(port, () => {
